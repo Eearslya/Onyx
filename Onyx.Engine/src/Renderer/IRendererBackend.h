49c44da2795add03d754ccd9868b4f021418d239
Eearslya Sleiarion <eearslya@gmail.com>
2020-06-24 09:20:30 -0700

    Add generic Renderer backend interface

---
+++ Onyx.Engine/src/Renderer/IRendererBackend.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Core/Defines.h"
+#include "Core/Types.h"
+
+namespace Onyx {
+class IApplication;
+
+//! Generic RendererBackend interface, to be implemented by different graphics APIs.
+class IRendererBackend {
+ public:
+  //! Construct our renderer backend, optionally with validation.
+  /*!
+    \param application The application this renderer should belong to.
+    \param enableValidation Indicates whether to add additional debug/validation calls
+                            to our renderer.
+  */
+  IRendererBackend(IApplication* application, const bool enableValidation) {}
+  virtual ~IRendererBackend() = default;
+
+  //! Returns whether validation is enabled for this backend.
+  /*!
+    \return true if validation is enabled, false otherwise.
+  */
+  virtual const bool ValidationEnabled() const = 0;
+};
+}  // namespace Onyx
